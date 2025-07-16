29cf36ac2d2bcedaf4a3c55ad2771bf07148b920
godnewface <gede.hate@gmail.com>
2025-07-16 16:51:50 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,32 +6,7 @@ module.exports = {
     "./music/**/*.html",
     "./music/music-data.json"
   ],
-  safelist: [
-    'mix',
-    'metal',
-    'punk',
-    'reggae',
-    'k-pop',
-    'fade-in-up',
-    'col-lg-4',
-    'col-md-6',
-    'btn',
-    'btn-outline-primary',
-    'text-center',
-    'modal',
-    'modal-dialog',
-    'modal-xl',
-    'modal-header',
-    'modal-title',
-    'modal-body',
-    'btn-close',
-    'list-inline',
-    'list-inline-item',
-    'shadow-sm',
-    'rounded',
-    'border',
-    'p-3',
-    'mb-4'
+  safelist: [ 
   ],
   theme: {
     extend: {},
