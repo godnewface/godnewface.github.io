4bbc111cae0d02c2e43b56e26e4d5b002f268b93
godnewface <gede.hate@gmail.com>
2025-07-16 13:55:03 +0700

    add purge

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./js/**/*.js",
+    "./music/**/*.html",
+    "./music/music-data.json",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
